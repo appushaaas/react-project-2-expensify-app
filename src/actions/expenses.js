6f479e6a430de7ec7c93a86a24f040bec3241069
Aparna S <1068@trenser.com>
2018-11-28 13:46:23 +0530

    Set up

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -36,7 +36,7 @@ export const removeExpense = (
 export const startRemoveExpense = ({ id } = {}) => {
    return(dispatch, getState) => {
       const uid = getState().auth.uid;
-       return database.ref("users/"+uid+"expenses/"+id).remove().then((response) => {
+       return database.ref(`users/${uid}/expenses/${id}`).remove().then((response) => {
          dispatch(removeExpense({ id }));
        });
    };
@@ -54,7 +54,7 @@ export const editExpense = (id, updates) => (
 export const startEditExpense = (id, updates) => {
    return(dispatch, getState) => {
       const uid = getState().auth.uid;
-      return database.ref("users/"+uid+"expenses/"+id).update(updates).then(() => {
+      return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(() => {
          dispatch(editExpense(id, updates));
       });
    };
